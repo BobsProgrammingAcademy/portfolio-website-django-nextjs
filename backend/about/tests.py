e29964a20b5072e62e434a22bdfcf5ced1da9cb2
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-05-24 14:21:34 +0200

    Added unit tests

--- backend/about/tests.py
+++ backend/about/tests.py
@@ -1,3 +1,15 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .models import About
 
-# Create your tests here.
+
+class AboutModelUnitTestCase(TestCase):
+    def setUp(self):
+        self.about = About.objects.create(
+            title='Lorem ipsum dolor sit amet',
+            description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt quis odio id.',
+            icon='settings'
+        )
+
+    def test_about_model(self):
+        data = self.about
+        self.assertIsInstance(data, About)
