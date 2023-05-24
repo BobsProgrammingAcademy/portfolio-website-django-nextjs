104f1a0413eb1708562ab089ce7a214c3f4425a7
BobsProgrammingAcademy <bobsprogrammingacademy@gmail.com>
2023-05-24 14:24:08 +0200

    Updated unit tests

--- backend/hero/tests.py
+++ backend/hero/tests.py
@@ -1,3 +1,17 @@
-# from django.test import TestCase
+import tempfile
+from django.test import TestCase
+from .models import Hero
 
-# Create your tests here.
+
+class HeroModelUnitTestCase(TestCase):
+    def setUp(self):
+        self.hero = Hero.objects.create(
+            title='Lorem ipsum dolor sit amet',
+            subtitle='Sed tincidunt quis odio id molestie',
+            description='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt quis odio id.',
+            image=tempfile.NamedTemporaryFile(suffix='.jpg').name
+        )
+
+    def test_hero_model(self):
+        data = self.hero
+        self.assertIsInstance(data, Hero)
