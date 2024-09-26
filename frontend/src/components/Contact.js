501000ba569651fbc4bd5471bc0cf88ed27a892b
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2024-09-26 13:56:56 +0200

    Code refactoring

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Box from '@mui/material/Box';
