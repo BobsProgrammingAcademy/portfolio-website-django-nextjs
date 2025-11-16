f444dfeeed095b2b861ee4e38356fe35b026555a
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2025-11-16 14:54:57 +0100

    Code refactoring

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 
+// Components
 import createEmotionCache from '../utils/createEmotionCache';
 
 export default class MyDocument extends Document {
